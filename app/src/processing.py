2a99a61d295a6f42e1e50a5d669f1eeabf87cf0a
Lexxo <lexx.orlov@gmail.com>
2023-05-21 21:48:25 +0400

    moved behaviour out of File dataclass

--- app/src/processing.py
+++ app/src/processing.py
@@ -21,19 +21,11 @@ def get_folder_table(folder: str) -> pd.DataFrame:
     return files_table
 
 
-def search_file(folder: pd.DataFrame, name: str) -> dict:
-
-    filtered = folder[folder['name'].str.contains(name)]
-    file = filtered.sort_values(by='modifiedTime', ascending=False).iloc[0]
-    file = dict(file)
-
-    return file
-
-
 @dataclass
 class File:
 
     id: str
+    # modified: date
     name: str
 
     def _format_dataframe(self):
@@ -53,27 +45,39 @@ class File:
     def __repr__(self):
         return f'File ({self.name}, {self.id}, r/c: {self.data.shape})'
 
-    def monthly_cumulative_expenses(self, accs: list, month_offset=0):
-        df = self.data[self.data[Columns.ACC].isin(accs)]
 
-        start = date.today() + pd.tseries.offsets.MonthBegin(-2 + month_offset)
-        end = date.today() + pd.tseries.offsets.MonthEnd(month_offset)
+def monthly_cumulative_expenses(file: File, accs: list, month_offset=0):
+    df = file.data[file.data[Columns.ACC].isin(accs)]
 
-        grid = pd.DataFrame(pd.date_range(start, end), columns=[Columns.DATE])
+    start = date.today() + pd.tseries.offsets.MonthBegin(-2 + month_offset)
+    end = date.today() + pd.tseries.offsets.MonthEnd(month_offset)
 
-        df_expense = df[~df[Columns.CAT].isin(['Income', 'Account Transfer'])]
-        df_expense[Columns.AMOUNT] = df[Columns.AMOUNT] * -1
+    grid = pd.DataFrame(pd.date_range(start, end), columns=[Columns.DATE])
 
-        df_month = grid.merge(df_expense.groupby(Columns.DATE).sum()[Columns.AMOUNT].reset_index(),
-                              on=['Date'], how='left')
-        df_month.fillna(0, inplace=True)
+    df_expense = df[~df[Columns.CAT].isin(['Income', 'Account Transfer'])]
+    df_expense[Columns.AMOUNT] = df[Columns.AMOUNT] * -1
 
-        df_month['month'] = df_month[Columns.DATE].dt.month
-        df_month['day'] = df_month[Columns.DATE].dt.day
+    df_month = grid.merge(df_expense.groupby(Columns.DATE).sum()[Columns.AMOUNT].reset_index(),
+                          on=['Date'], how='left')
+    df_month.fillna(0, inplace=True)
 
-        df_month['runtot'] = df_month[['month', Columns.AMOUNT]].groupby('month').cumsum()
+    df_month['month'] = df_month[Columns.DATE].dt.month
+    df_month['day'] = df_month[Columns.DATE].dt.day
 
-        df_month.loc[df_month['month'] == end.month, 'period'] = 'current'
-        df_month.loc[df_month['month'] == start.month, 'period'] = 'previous'
+    df_month['runtot'] = df_month[['month', Columns.AMOUNT]].groupby('month').cumsum()
 
-        return df_month
+    df_month.loc[df_month['month'] == end.month, 'period'] = 'current'
+    df_month.loc[df_month['month'] == start.month, 'period'] = 'previous'
+
+    return df_month
+
+
+def search_file(folder: pd.DataFrame, name: str) -> File:
+
+    filtered = folder[folder['name'].str.contains(name)]
+    if filtered.shape[0] == 0:
+        raise FileNotFoundError('File not found in Google Drive')
+    meta = filtered.sort_values(by='modifiedTime', ascending=False).iloc[0]
+    out = File(id=meta['id'], name=meta['name'])
+
+    return out
--- app/src/run_test.py
+++ app/src/run_test.py
@@ -1,6 +1,6 @@
 from config import config
 
-from processing import get_folder_table, search_file, File
+from processing import get_folder_table, search_file, monthly_cumulative_expenses
 
 from charts import plot_mom
 
@@ -11,12 +11,14 @@ if __name__ == '__main__':
 
     df_folder = get_folder_table(expense_folder)
 
-    file = search_file(df_folder, '.*master.*')
+    trx_aed = search_file(df_folder, '.*aed.csv')
+    trx_nonaed = search_file(df_folder, '.*infrequent.csv')
+    trx_closed = search_file(df_folder, '.*closed.*.csv')
 
-    test_file = File(id=file['id'], name=file['name'])
+    expenses = monthly_cumulative_expenses(file=trx_aed,
+                                           accs=['Credit ENBD', 'AED ENBD', 'Cash AED', 'Capital AED'],
+                                           month_offset=-5)
 
-    expenses = test_file.monthly_cumulative_expenses(accs=['Credit ENBD', 'AED ENBD', 'Cash AED', 'Capital AED'], month_offset=-5)
-
-    fig = plot_mom(expenses)
+    # fig = plot_mom(expenses)
 
     print('Done!')
