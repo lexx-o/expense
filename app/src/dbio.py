bc763e8b3c255058a43da4b6753c40f55104a738
Lexxo <lexx.orlov@gmail.com>
2023-06-27 16:57:25 +0400

    added command to partially update tables per account

--- app/src/dbio.py
+++ app/src/dbio.py
@@ -1,11 +1,34 @@
+from dataclasses import dataclass
+
 import pandas as pd
+from sqlalchemy import Engine, text
+
 from config import config
-
+from config.variables import Columns
 from driveio import File
-from sqlalchemy import Engine
 
 
-def write_table_update_log(*, partition: str, file: File, engine) -> None:
+@dataclass(frozen=True)
+class Table:
+    """A representation of PostgresDB tables"""
+    name: str
+    schema: str
+
+    def __str__(self):
+        return f"{self.schema}.{self.name}"
+
+    def write(self, df: pd.DataFrame, engine: Engine):
+        df.to_sql(name=self.name,
+                  con=engine,
+                  schema=self.schema,
+                  index=False,
+                  if_exists='replace')
+
+    def read(self, engine: Engine):
+        return pd.read_sql(sql=f"SELECT * FROM {self}", con=engine)
+
+
+def write_table_update_log(*, partition: str, file: File, engine: Engine) -> None:
     """
     Writes to table of expense partition table update log.
     Args:
@@ -30,3 +53,49 @@ def read_table_update_log(engine: Engine) -> pd.DataFrame:
         DataFrame of update_log table
     """
     return pd.read_sql(sql=f"SELECT * FROM {config.schema}.update_log", con=engine)
+
+
+def get_latest_log(log: pd.DataFrame) -> pd.DataFrame:
+    df = pd.DataFrame(index=config.partitions.keys())
+    latest = log.sort_values(by=['dttm'], ascending=False).groupby(by=['partition']).last()
+    modified_times = df.join(latest)
+    modified_times['modified'] = pd.to_datetime(modified_times['modified'])
+    return modified_times
+
+
+def update_table(data: pd.DataFrame, table: Table, engine: Engine):
+    """
+    Updates table from a dataframe.
+    Entries that have accounts found in the dataframe are cleared and then appended to the source table.
+    """
+
+    target = Table(name='_temp_target', schema=config.schema)
+    target.write(data, engine=engine)
+
+
+    query = f"""
+        begin;
+    
+        create table {config.schema}._temp_result as
+        select * 
+        from {table} as source
+        where source.\"{Columns.ACC}\" NOT IN
+            (
+            select distinct \"{Columns.ACC}\"
+            from {target}
+            )
+        union all
+        select *
+        from {config.schema}._temp_target;
+        
+        alter table {table} rename to {table.name}_old;
+        alter table {config.schema}._temp_result rename to {table.name};
+        
+        drop table {table}_old;
+        drop table {config.schema}._temp_target;
+        
+        commit;
+    """
+
+    with engine.connect() as con:
+        con.execute(text(query))
