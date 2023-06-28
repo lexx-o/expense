fbfaf8fa5083b0f2552bc36a993a408ffe4861bf
Lexxo <lexx.orlov@gmail.com>
2023-06-28 12:11:23 +0400

    added docstring to date-trimming func

--- app/src/util.py
+++ app/src/util.py
@@ -2,6 +2,14 @@ import pandas as pd
 
 
 def trim_date_and_remove_tz(dates: pd.Series, trim: 'str' = 's') -> pd.Series:
+    """
+    Transforms a Series of date-like data into timezone-naive pd.Timestamp type and rounds to seconds
+    Args:
+        dates: Series containing dates
+        trim: rounding period
+    Returns:
+        Series of pd.Timestamp data
+    """
     dates1 = pd.to_datetime(dates)
     dates2 = dates1.dt.tz_localize(None)
     dates3 = dates2.dt.round(trim)
