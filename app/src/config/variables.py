aa5573f9964b877d59110037054553b8d0cf73fd
Lexxo <lexx.orlov@gmail.com>
2023-07-11 23:01:16 +0400

    updated Columns variables

--- app/src/config/variables.py
+++ app/src/config/variables.py
@@ -1,16 +1,19 @@
-from enum import Enum
 from dataclasses import dataclass
+from config import config
 
 
 @dataclass(frozen=True)
 class Columns:
-    DATE = 'Date'
+    DATE = config.schema.date
+    ACC = config.schema.account
+    AMOUNT = config.schema.amount
+    CAT = config.schema.lvl1
+    CAT2 = config.schema.lvl2
+    DESC = config.schema.text
+
     YEAR = 'year'
     MONTH = 'month'
     DAY = 'day'
-    AMOUNT = 'Amount'
-    CAT = 'Category'
-    ACC = 'Account'
 
 
 @dataclass(frozen=True)
